2d73cb9a7c6028fda42e399abe388f81f6d67cd2
Niraj1903 <niraj.singh1903@outlook.com>
2025-11-27 18:14:13 +0530

    server setup

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const express = require("express"); // this is getting express from node modules
+
+const app = express(); // this is calling express js application / instance of express js application
+
+app.listen(3000); // app is listrning to 3000 port
