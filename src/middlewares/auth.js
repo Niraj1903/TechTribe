bf12d8491eafa821592551f977ea36c850f871f9
Niraj1903 <niraj.singh1903@outlook.com>
2025-11-29 22:36:33 +0530

    mongoose

---
+++ src/middlewares/auth.js
@@ -0,0 +1,12 @@
+// const auth = "XYZ";
+
+// const login = (req, res, next) => {
+//   const token = auth === "XYZ";
+//   if (!token) {
+//     res.status(401).send("Error 401");
+//   } else {
+//     next();
+//   }
+// };
+
+// module.exports = { login };
