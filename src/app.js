74f618335f7d47e1bc37617919ffb65c43e6e34f
Niraj1903 <niraj.singh1903@outlook.com>
2025-11-30 03:11:16 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ const express = require("express"); // this is getting express from node modules
 
 const app = express(); // this is calling express js application / instance of express js application
 require("./config/database");
+const { User } = require("./models/user");
 
 const { connectDatabase } = require("./config/database");
+
+app.post("/signUp", async (req, res) => {
+  const userObj = {
+    firstName: "Niraj",
+    lastName: "Singh",
+    gender: "M",
+    emailId: "nirajsingh@test.com",
+    password: "test.com",
+    age: 55,
+  };
+  const user = new User(userObj);
+  await user.save();
+  res.send("user data added succesfully");
+});
 
 connectDatabase()
   .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   await mongoose.connect(
-    "mongodb+srv://Niraj:ZECz4ISfftgOo00U@myproject.zsaagvb.mongodb.net/"
+    "mongodb+srv://Niraj:ZECz4ISfftgOo00U@myproject.zsaagvb.mongodb.net/TechTribe"
   );
 };
 
